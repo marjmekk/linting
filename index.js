d98139a9133fd596b0d944f0a73362dcbb1e0120
Marjorie <114688337+marjmekk@users.noreply.github.com>
2023-03-07 23:04:57 +0000

    added 3 ejs file and pictures in each page

--- index.js
+++ index.js
@@ -1,5 +1,43 @@
-var http = require("http");
-http.createServer(function(req,res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Aloha world');
-}).listen(process.env.PORT || 3000);
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+
+//this will allow us to serve up static files, CSS, images & JS
+app.use(express.static(__dirname));
+
+
+app.get('/', (req, res) => {
+  var title = "Home Page";
+  res.render('pages/index',{'title':title});
+});
+
+app.get('/hiking', (req, res) => {
+  var title = "Hiking";
+  res.render('pages/hiking',{'title':title});
+});
+
+app.get('/food', (req, res) => {
+  var title = "Food";
+  res.render('pages/food',{'title':title});
+});
+
+app.get('/traveling', (req, res) => {
+  var title = "Traveling";
+  res.render('pages/traveling',{'title':title});
+});
+
+app.get('/projects', (req, res) => {
+  var title = "Projects Page";
+  res.render('pages/projects',{'title':title});
+});
+
+app.get('/contact', (req, res) => {
+  var title = "Contact Page";
+  res.render('pages/contact',{'title':title});
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
